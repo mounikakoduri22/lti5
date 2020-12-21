2a6910ab77827a7508da372b03194fec9daa4f35
mounikakoduri22 <bunni22@gmail.com>
2020-12-21 15:19:14 +0530

    modified

--- Demo.java
+++ Demo.java
@@ -1,13 +1,13 @@
 public class Demo {
 
 public static void main(String args[]){
-	int i;
+
 
 System.out.println("Hello World");
 
-for(i=0; i<=10; i++)
+for(i=1; i<=10; i++)
 {
-	System.out.println(i +"");
+	System.out.println(i);
 }
 	
 
